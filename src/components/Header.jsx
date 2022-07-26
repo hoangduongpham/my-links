48101e0adc1e7aeb077f83d05464c785906ee177
hoangduongpham <hoangduongpham2001@gmail.com>
2022-07-26 16:40:53 +0700

    Done task: Create header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
-import '../assets/css/header.css';
+import React from "react";
+import "../assets/css/header.css";
+import profilePic from "../assets/img/profile.jpg";
 
 const Header = () => {
-	return (
-        <header className='header'>
+    return (
+        <header className="header">
+            <img
+                className="profile-pic"
+                src={profilePic}
+                alt="Profile picture"
+            />
+            <h1 className="name">Phạm Hoàng Dương</h1>
+            <p className="description">
+                Final year university student; web developer
+            </p>
         </header>
-	);
+    );
 };
 
 export default Header;
