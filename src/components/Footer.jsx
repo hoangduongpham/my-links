f74271cef03e9543afd654be45ef5804d4b0df9f
hoangduongpham <hoangduongpham2001@gmail.com>
2022-07-28 11:25:02 +0700

    Done task: Create footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
-import '../assets/css/footer.css';
+import React from "react";
+import "../assets/css/footer.css";
 
 const FooterLink = () => {
-	return (
-		<footer className='footer'>
-		</footer>
-	);
+    return (
+        <footer className="footer">
+            © 2022 Copyright: Phạm Hoàng Dương
+            <br />
+            Icons made by{" "}
+            <a
+                href="https://www.flaticon.com/authors/freepik"
+                target="_blank"
+                rel="noreferrer"
+            >
+                Freepik
+            </a>{" "}
+            from{" "}
+            <a
+                href="https://www.flaticon.com/"
+                target="_blank"
+                rel="noreferrer"
+            >
+                www.flaticon.com
+            </a>
+        </footer>
+    );
 };
 
 export default FooterLink;
